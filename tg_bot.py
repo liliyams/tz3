80f0037d4a4e01bf5aaa533bec6a7f7dcebcd309
liliyams <whoisliliia@gmail.com>
2021-12-19 14:32:23 +0300

    bot handler added

--- tg_bot.py
+++ tg_bot.py
@@ -4,7 +4,10 @@ token = os.environ['TOKEN']
 my_id = os.environ['ID']
 bot = telebot.TeleBot(token)
 
+f = open("results.txt", "r")
+data = f.read()
+
 photo_url = "https://st3.depositphotos.com/6998422/18874/i/600/depositphotos_188741414-stock-photo-like-3d.jpg"
-bot.send_photo(my_id, photo=photo_url, caption='все работает')
+bot.send_photo(my_id, photo=photo_url, caption=str(data))
 
 
