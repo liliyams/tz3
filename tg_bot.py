aa149b36f9bcd0e9cf8a200e883113a14ce01a32
liliyams <whoisliliia@gmail.com>
2021-12-19 15:30:30 +0300

    done

--- tg_bot.py
+++ tg_bot.py
@@ -4,10 +4,15 @@ token = os.environ['TOKEN']
 my_id = os.environ['ID']
 bot = telebot.TeleBot(token)
 
-f = open("results.txt", "r")
-data = f.read()
+try:
+    f = open("bot.txt", "r")
+    data = f.read()
+    photo_url = "https://lh3.googleusercontent.com/proxy/7huI_QgzjiNTeqK3fXr5JMgZZ6pXwIOkOjf0_MEyTcMNDPumwmnTdgvgL3ZxMEp0L3QhQbpWwlaO9XbouUX9P1qGwfjuyWo133k"
+    bot.send_photo(my_id, photo=photo_url, caption=str(data))
 
-photo_url = "https://st3.depositphotos.com/6998422/18874/i/600/depositphotos_188741414-stock-photo-like-3d.jpg"
-bot.send_photo(my_id, photo=photo_url, caption=str(data))
+except FileNotFoundError as e:
+    data = "все в порядке"
+    photo_url = "https://st3.depositphotos.com/6998422/18874/i/600/depositphotos_188741414-stock-photo-like-3d.jpg"
+    bot.send_photo(my_id, photo=photo_url, caption=str(data))
 
 
